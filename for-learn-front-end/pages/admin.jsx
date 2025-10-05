4d7bb0f1606bc346ed383b774eb57cc9e5904ab3
thimansa911 <dinthi150@gmail.com>
2025-10-05 17:46:38 +0100

    day 10 ( part 2 ) 📎

--- for-learn-front-end/pages/admin.jsx
+++ for-learn-front-end/pages/admin.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function AdminPage(){
     return(
-        <div className="w-full h-full bg-white">
+        <div className="w-full h-full bg-white flex">
             <div className="w-[300px] h-full bg-gray-300">
-
+            </div>
+            <div className="w-[calc(100%-300px)] bg-blue-400 h-full">
+                <Routes path="/">
+                    <Route path="/dashboard" element={<span>Dashboard</span>}/>
+                    <Route path="/packages" element={<h1>Packages</h1>}/>
+                    <Route path="/orders" element={<h1>Orders</h1>}/>
+                </Routes>
             </div>
         </div>
     )
